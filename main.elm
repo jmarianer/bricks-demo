5d785bd0020c6d464dee337c9437cfd3ed2b9770
Joey Marianer <roie.marianer@gmail.com>
2017-10-11 15:26:35 -0700

    Remove buttons

--- main.elm
+++ main.elm
@@ -10,6 +10,17 @@ import Style
 main = Html.beginnerProgram { model = model, view = view, update = update }
 
 
+-- UTILS
+remove : Int -> Array a -> Array a
+remove i a =
+  let
+    a1 = Array.slice 0 i a
+    a2 = Array.slice (i+1) (Array.length a) a
+  in
+    Array.append a1 a2
+
+
+
 -- MODEL
 type alias Model = Array {
   rotate: (Int, Int, Int),
@@ -21,13 +32,14 @@ model = Array.empty
 
 -- UPDATE
 type Msg
-  = Add
+  = Add | Remove Int
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Add -> Array.push {rotate = (0,0,0), translate = (0,0,0)} model
+    Remove i -> remove i model
 
 -- VIEW
 styleSheet = Style.styleSheet [ ]
@@ -35,5 +47,12 @@ view : Model -> Html Msg
 view model =
   Element.layout styleSheet <|
     row {} [padding 50, spacing 50]
-      ([ button {} [padding 20, onClick Add] (text "a") ] ++ Array.toList (Array.map (text << toString) model))
+      ([ button {} [padding 20, onClick Add] (text "+") ] ++ Array.toList (Array.indexedMap viewItem model))
+
+
+viewItem i item =
+  column {} []
+    [text <| toString item,
+     button {} [onClick <| Remove i] (text "-")
+    ]
   
