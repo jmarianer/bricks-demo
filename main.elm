1ad9b56adbcc59109ea549a22f9f2133ed2e911c
Joey Marianer <roie.marianer@gmail.com>
2017-10-11 15:57:14 -0700

    Change things

--- main.elm
+++ main.elm
@@ -1,4 +1,5 @@
 import Array exposing (Array)
+import Debug
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Element.Events exposing (onClick)
@@ -11,6 +12,11 @@ main = Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- UTILS
+fromJust : Maybe a -> a
+fromJust x = case x of
+    Just y -> y
+    Nothing -> Debug.crash "error: fromJust Nothing"
+
 remove : Int -> Array a -> Array a
 remove i a =
   let
@@ -38,7 +44,13 @@ update msg model =
   case msg of
     Add -> Array.push (Array.fromList [0,0,0,0,0,0]) model
     Remove i -> remove i model
-    Modify i j add -> model
+    Modify i j add ->
+      let
+        oldArray = fromJust <| Array.get i model
+        oldVal = fromJust <| Array.get j oldArray
+        newArray = Array.set j (oldVal+add) oldArray
+      in
+        Array.set i newArray model
 
 -- VIEW
 styleSheet = Style.styleSheet [ ]
@@ -50,7 +62,17 @@ view model =
 
 viewItem : Int -> Item -> Element {} variation Msg
 viewItem i item =
-  column {} [] (
-    (List.concat <| List.map2 (\name val -> [text name, text <| toString val]) names <| Array.toList item)
-    ++ [button {} [onClick <| Remove i] (text "-")])
-  
+  let
+    numNameVal : List (Int, String, Int)
+    numNameVal = List.map2 (\val (num, name) -> (num, name, val)) (Array.toList item) (List.indexedMap (,) names)
+
+    showVal num val = row {} [spread] [
+      button {} [onClick <| Modify i num -1] (text "-"),
+      text <| toString val,
+      button {} [onClick <| Modify i num  1] (text "+")
+    ]
+  in
+    column {} [] (
+      (List.concatMap (\(num, name, val) -> [text name, showVal num val]) numNameVal)
+      ++ [button {} [onClick <| Remove i] (text "-")])
+    
