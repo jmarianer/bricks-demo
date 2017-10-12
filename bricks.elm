ec591aff920b4a798ddbfa6fc52f811e4ffd9c4d
Joey Marianer <roie.marianer@gmail.com>
2017-10-12 13:25:22 -0700

    Temp hack

--- bricks.elm
+++ bricks.elm
@@ -2,33 +2,55 @@ import Board exposing (Board, Orientation(..))
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Board exposing (Board, Orientation(..))
+import Time exposing (Time, second)
 import ShowBoard
 
-main = Html.beginnerProgram { model = model, view = view, update = update }
+-- Utilities
+fromJust : Maybe a -> a
+fromJust x = case x of
+    Just y -> y
+    Nothing -> Debug.crash "error: fromJust Nothing"
 
--- MODEL
+
+-- Hardcoded steps to the solution
 myTest =  "334;2112Y;2010Y;2200Y;2101Z"
 myTest1 = "334;2112Y;2010Y;2200Y;2100Z"
 myTest2 = "334;2102Y;2010Y;2200Y;2100Z"
 
-model : Board
-model = Board.toBoard myTest
+steps = ["334;2112Y;2010Y;2200Y;2101Z",
+         "334;2112Y;2010Y;2200Y;2100Z",
+         "334;2102Y;2010Y;2200Y;2100Z"]
+
+main =
+  Html.program {
+    init = (Board.toBoard <| fromJust <| List.head steps, Cmd.none),
+    view = view,
+    update = update,
+    subscriptions = \_ -> Time.every second Tick
+  }
+
 
 -- UPDATE
-type alias Msg = Int
-update : Msg -> Board -> Board
-update msg model = case msg of
-  0 -> Board.toBoard myTest
-  1 -> Board.toBoard myTest1
-  2 -> Board.toBoard myTest2
-  _ -> model
+type Msg = Tick Time
+update : Msg -> Board -> (Board, Cmd Msg)
+update (Tick i) model =
+  let
+    newModel = case i of
+      0 -> Board.toBoard myTest
+      1 -> Board.toBoard myTest1
+      2 -> Board.toBoard myTest2
+      _ -> model
+  in
+    (newModel, Cmd.none)
+
 
 -- VIEW
 view : Board -> Html Msg
 view model =
   div [style [("display", "flex"), ("align-items", "center"), ("width", "100%"), ("height", "100%"), ("justify-content", "center")]] [
     ShowBoard.toHtml model,
-    button [onClick 0] [text "0"],
-    button [onClick 1] [text "1"],
-    button [onClick 2] [text "2"]
+    button [onClick <| Tick 0] [text "0"],
+    button [onClick <| Tick 1] [text "1"],
+    button [onClick <| Tick 2] [text "2"]
   ]
