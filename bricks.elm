a91d1191bab5a402d1ce9748f8bad7185e1f8cd9
Joey Marianer <roie.marianer@gmail.com>
2017-10-11 18:45:27 -0700

    oof

--- bricks.elm
+++ bricks.elm
@@ -29,10 +29,11 @@ update msg model = model
 -- VIEW
 view : Model -> Html Msg
 view model =
-  div [style [("transform", "rotateX(76deg) rotateY(16deg) rotateZ(30deg) translateX(1024px)"), ("transform-style", "preserve-3d"), ("transform-origin", "bottom left"), ("box-sizing", "border-box")]] (
-    box 0 0 0 model.width model.height model.depth []
-    ++ box 1 1 3 2 2 5 [("background-color", "blue"), ("opacity", "0.5")]
-  )
+  div [style [("display", "flex"), ("align-items", "center"), ("width", "100%"), ("height", "100%"), ("justify-content", "center")]] [
+    div [style [("transform", "rotateX(76deg) rotateY(16deg) rotateZ(30deg)"), ("transform-style", "preserve-3d"), ("transform-origin", "bottom left"), ("box-sizing", "border-box")]] (
+      box 0 0 0 model.width model.height model.depth []
+      ++ box 1 1 3 2 3 6 [("background-color", "blue"), ("opacity", "0.5")]
+  )]
 
 standardStyles = [("transform-origin", "top left"), ("border", "1px solid black"), ("position", "absolute"), ("box-sizing", "border-box")]
 
@@ -57,14 +58,13 @@ transform o x y z =
   in
     ("transform", transform)
 
-color = [("background-color", "red"), ("opacity", "0.5")]
 box x1 y1 z1 x2 y2 z2 styles = 
   let
     otherStyles = styles ++ standardStyles
   in
   [
-    div [style ([width (y2 - y1), height (x2 - x1), transform Z x1 y1 z1] ++ color ++ otherStyles)] [],
-    div [style ([width (y2 - y1), height (x2 - x1), transform Z x1 y1 z2] ++ color ++ otherStyles)] [],
+    div [style ([width (y2 - y1), height (x2 - x1), transform Z x1 y1 z1] ++ otherStyles)] [],
+    div [style ([width (y2 - y1), height (x2 - x1), transform Z x1 y1 z2] ++ otherStyles)] [],
     div [style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y1] ++ otherStyles)] [],
     div [style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y2] ++ otherStyles)] [],
     div [style ([width (y2 - y1), height (z2 - z1), transform X y1 z1 x1] ++ otherStyles)] [],
