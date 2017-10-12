1664cf5f661ae560b24f28548774d57a1ba4c1ce
Joey Marianer <roie.marianer@gmail.com>
2017-10-12 13:13:05 -0700

    ShowBoard

---
+++ ShowBoard.elm
@@ -0,0 +1,79 @@
+module ShowBoard exposing (toHtml)
+
+import Board exposing (Board, Block, Orientation(..))
+import Html exposing (Html, div)
+import Html.Attributes
+
+-- Hardcoded styles. TODO: Learn elm-css
+type alias StyleElement = (String, String)
+type alias Style = List StyleElement
+mainDivStyle : Style
+mainDivStyle = [
+  ("transform", "rotateX(76deg) rotateY(187deg) rotateZ(320deg)"),
+  ("transform-style", "preserve-3d"),
+  ("transform-origin", "bottom left"),
+  ("box-sizing", "border-box")]
+
+boxStyle : Style
+boxStyle = [
+  ("transform-origin", "top left"),
+  ("border", "1px solid black"),
+  ("position", "absolute"),
+  ("box-sizing", "border-box"),
+  ("transition", "all ease-in-out .3s")]
+
+mainBlockStyle : Style
+mainBlockStyle = boxStyle ++ [("background-color", "red")]
+
+otherBlockStyle : Style
+otherBlockStyle = boxStyle ++ [("background-color", "blue"), ("opacity", "0.5")]
+
+
+-- Styling utilities
+pixelsPerBlock = 50
+toPixels x = toString (pixelsPerBlock * x) ++ "px"
+
+width : Int -> StyleElement
+width x = ("width", toPixels x)
+
+height : Int -> StyleElement
+height y = ("height", toPixels y)
+
+transform : Orientation -> Int -> Int -> Int -> StyleElement
+transform orientation x y z = 
+  let
+    translate x y z = String.concat <| List.concat <| List.map2 (\d x -> ["translate", d, "(", toPixels x, ")"]) ["X", "Y", "Z"] [x, y, z]
+    transform = case orientation of
+      X -> "rotateX(-90deg)" ++ translate x y z
+      Y -> "rotateY(90deg)" ++ translate x y z
+      Z -> translate x y -z
+  in
+    ("transform", transform)
+
+
+-- Functions
+toHtml : Board -> Html msg
+toHtml board =
+  let
+    boundingBox = box 0 0 0 board.width board.depth board.height boxStyle
+    mainBlock = blockToBox board.mainBlock mainBlockStyle
+    otherBlocks = List.concatMap (\block -> blockToBox block otherBlockStyle) board.blocks
+  in
+    div [Html.Attributes.style mainDivStyle] (boundingBox ++ mainBlock ++ otherBlocks)
+  
+blockToBox : Block -> Style -> List (Html msg)
+blockToBox { length, x, y, z, orientation } =
+  case orientation of
+    X -> box x y z (x+length) (y+1) (z+1)
+    Y -> box x y z (x+1) (y+length) (z+1)
+    Z -> box x y z (x+1) (y+1) (z+length)
+
+box : Int -> Int -> Int -> Int -> Int -> Int -> Style -> List (Html msg)
+box x1 y1 z1 x2 y2 z2 style = [
+    div [Html.Attributes.style ([width (y2 - y1), height (x2 - x1), transform Z y1 x1 z1] ++ style)] [],
+    div [Html.Attributes.style ([width (y2 - y1), height (x2 - x1), transform Z y1 x1 z2] ++ style)] [],
+    div [Html.Attributes.style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y1] ++ style)] [],
+    div [Html.Attributes.style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y2] ++ style)] [],
+    div [Html.Attributes.style ([width (y2 - y1), height (z2 - z1), transform X y1 z1 x1] ++ style)] [],
+    div [Html.Attributes.style ([width (y2 - y1), height (z2 - z1), transform X y1 z1 x2] ++ style)] []
+  ]
--- bricks.elm
+++ bricks.elm
@@ -1,7 +1,8 @@
+import Board exposing (Board, Orientation(..))
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
-import Board exposing (Board, Orientation(..))
+import ShowBoard
 
 main = Html.beginnerProgram { model = model, view = view, update = update }
 
@@ -23,58 +24,11 @@ update msg model = case msg of
   _ -> model
 
 -- VIEW
-blockToBox : Board.Block -> List (String, String) -> List (Html msg)
-blockToBox { length, x, y, z, orientation } =
-  case orientation of
-    X -> box x y z (x+length) (y+1) (z+1)
-    Y -> box x y z (x+1) (y+length) (z+1)
-    Z -> box x y z (x+1) (y+1) (z+length)
-
 view : Board -> Html Msg
 view model =
   div [style [("display", "flex"), ("align-items", "center"), ("width", "100%"), ("height", "100%"), ("justify-content", "center")]] [
-    div [style [("transform", "rotateX(76deg) rotateY(187deg) rotateZ(320deg)"), ("transform-style", "preserve-3d"), ("transform-origin", "bottom left"), ("box-sizing", "border-box")]] (
-      box 0 0 0 model.width model.depth model.height []
-      ++ blockToBox model.mainBlock [("background-color", "red")]
-      ++ List.concatMap (\block -> blockToBox block [("background-color", "blue"), ("opacity", "0.5")]) model.blocks
-    ),
+    ShowBoard.toHtml model,
     button [onClick 0] [text "0"],
     button [onClick 1] [text "1"],
     button [onClick 2] [text "2"]
-  ]
-
-standardStyles = [("transform-origin", "top left"), ("border", "1px solid black"), ("position", "absolute"), ("box-sizing", "border-box"), ("transition", "all ease-in-out .3s")]
-
-pixelsPerBlock = 50
-toPixels x = toString (pixelsPerBlock * x) ++ "px"
-
-type alias Style = (String, String)
-width : Int -> Style
-width x = ("width", toPixels x)
-
-height : Int -> Style
-height y = ("height", toPixels y)
-
-transform : Orientation -> Int -> Int -> Int -> Style
-transform o x y z = 
-  let
-    translate x y z = String.concat <| List.concat <| List.map2 (\d x -> ["translate", d, "(", toPixels x, ")"]) ["X", "Y", "Z"] [x, y, z]
-    transform = case o of
-      X -> "rotateX(-90deg)" ++ translate x y z
-      Y -> "rotateY(90deg)" ++ translate x y z
-      Z -> translate x y -z
-  in
-    ("transform", transform)
-
-box x1 y1 z1 x2 y2 z2 styles = 
-  let
-    otherStyles = styles ++ standardStyles
-  in
-  [
-    div [style ([width (y2 - y1), height (x2 - x1), transform Z y1 x1 z1] ++ otherStyles)] [],
-    div [style ([width (y2 - y1), height (x2 - x1), transform Z y1 x1 z2] ++ otherStyles)] [],
-    div [style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y1] ++ otherStyles)] [],
-    div [style ([width (z2 - z1), height (x2 - x1), transform Y z1 x1 y2] ++ otherStyles)] [],
-    div [style ([width (y2 - y1), height (z2 - z1), transform X y1 z1 x1] ++ otherStyles)] [],
-    div [style ([width (y2 - y1), height (z2 - z1), transform X y1 z1 x2] ++ otherStyles)] []
   ]
