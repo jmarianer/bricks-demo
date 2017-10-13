fd891e7758a90d76d3a6b98a7d90889aa1c5f0d9
Joey Marianer <roie.marianer@gmail.com>
2017-10-13 15:25:37 -0700

    More helper functions in SolveBoard

--- SolveBoard.elm
+++ SolveBoard.elm
@@ -5,6 +5,7 @@ import Set exposing (Set)
 
 testBoard = toBoard "334;2112Y;2010Y;2200Y;2101Z"
 
+-- TODO: Better type names for Coords and Set Coords
 type alias Coords = (Int, Int, Int)
 
 coordinates : Block -> Coords
@@ -24,6 +25,21 @@ withCoord (x, y, z) orientation newCoord =
     Y -> (x, newCoord, z)
     Z -> (x, y, newCoord)
 
+advanceBy : Coords -> Orientation -> Int -> Coords
+advanceBy coords orientation delta =
+  let
+    oldCoord = getCoord coords orientation
+    newCoord = oldCoord + delta
+  in
+    withCoord coords orientation newCoord
+
+advanceBlockBy : Block -> Int -> Block
+advanceBlockBy block delta =
+  case block.orientation of
+    X -> { block | x = block.x + delta }
+    Y -> { block | y = block.y + delta }
+    Z -> { block | z = block.z + delta }
+
 spaces : Block -> Set Coords
 spaces block =
   let
@@ -33,19 +49,33 @@ spaces block =
   in
     Set.fromList <| List.map (\x -> withCoord coords block.orientation x) <| List.range start end
 
-
-
-allSpaces : Board -> Set (Int, Int, Int)
+allSpaces : Board -> Set Coords
 allSpaces board = List.foldl Set.union (spaces board.mainBlock) (List.map spaces board.blocks)
 
-{-
+spaceEmpty : Board -> Coords -> Bool
+spaceEmpty board (x, y, z) =
+  let
+    usedSpaces = allSpaces board
+  in
+    (x >= 0) && (y >= 0) && (z >= 0) &&
+    (x < board.width) && (y < board.depth) && (z < board.height) &&
+    (not <| Set.member (x, y, z) usedSpaces)
+
 move : Board -> Block -> List Block
 move board block =
-  case block.orientation of
-    X -> maybeMoveTo
+  let
+    coords = coordinates block
+    o = block.orientation
+    nextSpace = advanceBy coords o block.length
+    prevSpace = advanceBy coords o -1
+    moveIf space delta =
+      if spaceEmpty board space
+      then [advanceBlockBy block delta]
+      else []
+  in
+    (moveIf nextSpace 1) ++ (moveIf prevSpace -1)
 
 --nextMoves : Board -> List Board
 --nextMoves board =
 
 --solveBoard : Board -> List Board
--}
