d711e803f57dd6b6d1f64c8f4f9fcaf06decf297
Joey Marianer <roie.marianer@gmail.com>
2017-11-16 21:55:39 -0800

    Name it appropriately for cloud docker

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ docker run -v $(pwd)/client:/code -w "/code" mblouin/docker_lesscss style.less s
 docker run -v $(pwd)/client:/code -w "/code" codesimple/elm:0.18 make Main.elm --yes --output static/bricks.js
 docker build -f server/Dockerfile.haskell.cabal -t haskell-image .
 docker run -v $(pwd)/server:/code -w "/code" haskell-image ghc server.hs -optl-static -optl-pthread
-docker build -t bricks:latest .
+docker build -t joeym/bricks:latest .
 
 
 # Build and start:
-#   sh -e build.sh && docker run -p 80:80 -it bricks:latest
+#   sh -e build.sh && docker run -p 80:80 -it joeym/bricks:latest
 # Stop with ^C
